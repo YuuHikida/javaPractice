fd31a92ff500918153d367037698e25ba7602a52
hikida <ponponda0103@gmail.com>
2024-09-17 20:31:03 +0900

    shirotiri refactoring

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,12 @@ public class Main {
         // 自分の得意な言語で
         // Let's チャレンジ！！
         Scanner sc = new Scanner(System.in);
-        String[] x_y = sc.nextLine().split(" ");
-        String[] y_x = sc.nextLine().split(" ");
+//        String[] x_y = sc.nextLine().split(" ");
+        int x = Integer.parseInt(sc.nextLine());
+        int y = Integer.parseInt(sc.nextLine());
 
         // 最終出力用
-        int last_printf = Integer.parseInt(x_y[1]) + Integer.parseInt(y_x[1]);
-
+        System.out.println(x+y);
 
 
     }

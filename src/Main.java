99cd271f1f0727ad41893c9ff7647aaa9c99561b
hikida <ponponda0103@gmail.com>
2024-09-17 20:52:54 +0900

    pointa

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,26 @@ public class Main {
         // 自分の得意な言語で
         // Let's チャレンジ！！
         Scanner sc = new Scanner(System.in);
-//        String[] x_y = sc.nextLine().split(" ");
-        int x = Integer.parseInt(sc.nextLine());
-        int y = Integer.parseInt(sc.nextLine());
+        String[] crd_NumberOfTimes = sc.nextLine().split(" ");
+        int card = Integer.parseInt(crd_NumberOfTimes[0]);
+        int numberOfTimes = Integer.parseInt(crd_NumberOfTimes[1]);
+        int pointTotal = 0;
+        for(int i=0;i<numberOfTimes;i++)
+        {
+            int tmpPrice = Integer.parseInt(sc.nextLine());
+            if(pointTotal >tmpPrice)
+            {
+                pointTotal -= tmpPrice;
+            }else
+            {
+                card -= tmpPrice;
+                pointTotal += (int) (tmpPrice * 0.1);
+            }
+            System.out.println(card+" "+pointTotal);
+        }
 
         // 最終出力用
-        System.out.println(x+y);
-
-
+//        System.out.println();
     }
 }
 
